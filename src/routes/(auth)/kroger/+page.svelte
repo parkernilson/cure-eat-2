519c3a8a43802c5588e0b51afa6d182da6d62356
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-13 21:08:57 -0700

    delete some unused files

--- src/routes/(auth)/kroger/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-
-</script>
-
-<!-- Temp form for testing the kroger product search api -->
-<form method="GET">
-    <input name="search-term" placeholder="Search term" />
-    <button type="submit">Search</button>
-</form>
