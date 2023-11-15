6571826a719ea9312d9c3165155c7bd635532409
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-15 10:47:07 -0700

    Beautify the home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,15 @@
 
 </script>
 
-<h1>Cure Eat</h1>
-<p>Welcome to Cure Eat</p>
-<div class="h-full">
-    <slot />
+<div class="h-full flex flex-col">
+    <div class="flex-1">
+        <slot />
+    </div>
+
+    <!-- Footer -->
+    <div class="w-full h-8 bg-slate-200 flex justify-center items-center">
+        <p class="mr-3">Parker Nilson &copy; 2023</p>
+        <a href="https://github.com/parkernilson" class="hover:text-blue-600"><i class="fa-brands fa-github mr-2"></i></a>
+        <a href="https://www.linkedin.com/in/parker-nilson-8a5971253/" class="hover:text-blue-600"><i class="fa-brands fa-linkedin"></i>
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Main Page</h1>
+<div class="h-full flex flex-col items-center">
+    <h1 class="font-display text-3xl mt-40">Welcome to</h1>
+    <img class="w-82 mb-12" src="/assets/cure-eat-logo.png" alt="cure eat logo"/>
+    <div class="w-96 mb-8">
+        <h2 class="text-center font-body text-xl mb-6">Your friendly helper for grocery shopping</h2>
+        <p class="text-center font-body text-lg">Here, you can make shopping lists and automatically search for products at Kroger stores near you</p>
+    </div>
+    <a class="border px-3 text-lg" href="/login">Let's get started</a>
+</div>
