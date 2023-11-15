9dd325ca28e35c95447f9797b49dd94ba12b201d
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-15 11:07:04 -0700

    make login form look nice

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
-	import LoginRegisterCard from '$lib/components/ui/LoginRegisterCard.svelte';
-	import type { ActionData, PageData } from './$types';
+	import type { PageData } from './$types';
 
 	export let data: PageData;
-
 </script>
 
-{#if data?.user}
-	<p>logged in as {data.user.username}</p>
-{/if}
-
-<form method="POST">
-	<input name="username" type="text" placeholder="username">
-	<input name="password" type="password" placeholder="password">
-	<button type="submit">Login</button>
-</form>
+<div class="h-full flex items-center">
+	<div
+		class="w-2/3 max-w-md m-auto
+		px-3 py-8 pt-6
+		shadow-md
+		rounded-sm
+		bg-slate-100"
+	>
+		<form method="POST">
+			<h2 class="font-display text-6xl text-center mb-8">Login</h2>
+			<div class="flex flex-col justify-center">
+				<div class="mb-3">
+					<input
+						class="border border-black px-1 rounded-sm w-full"
+						name="username"
+						type="text"
+						placeholder="username"
+					/>
+				</div>
+				<div class="flex items-center mb-6">
+					<input
+						class="border border-black px-1 rounded-sm w-full"
+						name="password"
+						type="password"
+						placeholder="password"
+					/>
+				</div>
+				<div class="flex flex-col items-center">
+					<button
+						class="px-5 text-center cursor-pointer border border-black max-w-xs m-auto rounded-full"
+						type="submit">Login</button
+					>
+					<p class="text-center mt-6">
+						Need an account?
+						<a href="/register" class="text-blue-400 hover:text-blue-600"> Register here </a>
+					</p>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
