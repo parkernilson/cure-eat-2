4886d07b5d6799857df572030000155d019b25c5
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-20 23:31:07 -0700

    fix pb hooks to run in transactions to avoid race

--- pb_hooks/list-items-reorderable.pb.js
+++ pb_hooks/list-items-reorderable.pb.js
@@ -2,70 +2,72 @@
 
 onModelBeforeCreate((e) => {
 
-	const listItemsAfter = $app
-		.dao()
-		.findRecordsByExpr(
-            'list_items',
-            $dbx.exp('ordinal >= {:newOrdinal}', { newOrdinal: e.model.getInt('ordinal') }),
-            $dbx.exp('list = {:curList}', { curList: e.model.get('list') })
-		);
+    $app.dao().runInTransaction((txDao) => {
+        const listItemsAfter = txDao
+            .findRecordsByExpr(
+                'list_items',
+                $dbx.exp('ordinal >= {:newOrdinal}', { newOrdinal: e.model.getInt('ordinal') }),
+                $dbx.exp('list = {:curList}', { curList: e.model.get('list') })
+            );
 
-    listItemsAfter.forEach((item) => {
-        item.set('ordinal', item.getInt('ordinal') + 1)
-        $app.dao().saveRecord(item)
+        listItemsAfter.forEach((item) => {
+            item.set('ordinal', item.getInt('ordinal') + 1)
+            txDao.withoutHooks().saveRecord(item)
+        })
     })
 
 }, 'list_items');
 
 onModelAfterDelete((e) => {
-    
-    const listItemsAfter = $app
-        .dao()
-        .findRecordsByExpr(
+
+    $app.dao().runInTransaction((txDao => {
+        const listItemsAfter = txDao.findRecordsByExpr(
             'list_items',
             $dbx.exp('ordinal > {:curOrdinal}', { curOrdinal: e.model.getInt('ordinal') }),
             $dbx.exp('list = {:list}', { list: e.model.get('list') })
         )
 
-    listItemsAfter.forEach((item) => {
-        item.set('ordinal', item.getInt('ordinal') - 1)
-        $app.dao().saveRecord(item)
-    })
+        for (let item of listItemsAfter) {
+            item.set('ordinal', item.get('ordinal') - 1)
+            txDao.saveRecord(item)
+        }
+    }))
 
 }, 'list_items')
 
 onModelBeforeUpdate((e) => {
 
-    const originalCopy = e.model.originalCopy()
-    const oldOrdinal = originalCopy.get('ordinal')
-    const newOrdinal = e.model.get('ordinal')
+    $app.dao().runInTransaction((txDao) => {
+        const originalCopy = e.model.originalCopy()
+        const oldOrdinal = originalCopy.get('ordinal')
+        const newOrdinal = e.model.get('ordinal')
 
-    if (oldOrdinal < newOrdinal) {
-        const itemsBetweenAndIncludingNewOrdinal = $app.dao().findRecordsByExpr(
-            'list_items',
-            $dbx.exp('ordinal > {:oldOrdinal}', { oldOrdinal }),
-            $dbx.exp('ordinal <= {:newOrdinal}', { newOrdinal }),
-            $dbx.exp('list = {:list}', { list: e.model.get('list') })
-        )
+        if (oldOrdinal < newOrdinal) {
+            const itemsBetweenAndIncludingNewOrdinal = txDao.findRecordsByExpr(
+                'list_items',
+                $dbx.exp('ordinal > {:oldOrdinal}', { oldOrdinal }),
+                $dbx.exp('ordinal <= {:newOrdinal}', { newOrdinal }),
+                $dbx.exp('list = {:list}', { list: e.model.get('list') })
+            )
 
-        itemsBetweenAndIncludingNewOrdinal.forEach((item) => {
-            item.set('ordinal', item.get('ordinal') - 1)
-            $app.dao().withoutHooks().saveRecord(item)
-        })
+            itemsBetweenAndIncludingNewOrdinal.forEach((item) => {
+                item.set('ordinal', item.get('ordinal') - 1)
+                txDao.withoutHooks().saveRecord(item)
+            })
 
-    } else if (oldOrdinal > newOrdinal) {
-        const itemsBetweenAndIncludingNewOrdinal = $app.dao().findRecordsByExpr(
-            'list_items',
-            $dbx.exp('ordinal < {:oldOrdinal}', { oldOrdinal }),
-            $dbx.exp('ordinal >= {:newOrdinal}', { newOrdinal }),
-            $dbx.exp('list = {:list}', { list: e.model.get('list') })
-        )
+        } else if (oldOrdinal > newOrdinal) {
+            const itemsBetweenAndIncludingNewOrdinal = txDao.findRecordsByExpr(
+                'list_items',
+                $dbx.exp('ordinal < {:oldOrdinal}', { oldOrdinal }),
+                $dbx.exp('ordinal >= {:newOrdinal}', { newOrdinal }),
+                $dbx.exp('list = {:list}', { list: e.model.get('list') })
+            )
 
-        itemsBetweenAndIncludingNewOrdinal.forEach((item) => {
-            item.set('ordinal', item.get('ordinal') + 1)
-            $app.dao().withoutHooks().saveRecord(item)
-        })
-
-    }
+            itemsBetweenAndIncludingNewOrdinal.forEach((item) => {
+                item.set('ordinal', item.get('ordinal') + 1)
+                txDao.withoutHooks().saveRecord(item)
+            })
+        }
+    })
 
 }, 'list_items')
