3e4dd1958f3f428fad9c55eeb6687dbf5d7aa7b6
Parker Nilson <parker.todd.nilson@gmail.com>
2023-12-07 21:55:32 -0700

    set horizontal padding on the center column

--- apps/web/src/routes/(logged-in)/+layout.svelte
+++ apps/web/src/routes/(logged-in)/+layout.svelte
@@ -1,4 +1,4 @@
-<div class="h-full flex flex-col max-w-lg m-auto">
+<div class="h-full flex flex-col max-w-lg m-auto px-3">
     <div class="flex items-center justify-between pt-6">
         <img alt="cure eat logo" class="w-32" src="/assets/cure-eat-logo.png"/>
         <a href="/profile"><i class="fa-solid fa-circle-user fa-lg"></i></a>
